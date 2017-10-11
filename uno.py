2fb8479afe78d58801934c2fb0489763b523c338
MatPere <32298732+MatPere@users.noreply.github.com>
2017-10-11 10:57:26 +0000

    correction

--- uno.py
+++ uno.py
@@ -172,7 +172,7 @@ class Player():
         
     def play(self):
       print(self.main)
-      indice = input("indice de la carte à jouer  (-1 pour piocher/passer") :)
+      indice = input("indice de la carte à jouer  (-1 pour piocher/passer)" :)
       ActionCorrecte=False
       while ActionCorrecte==False:
         try :
